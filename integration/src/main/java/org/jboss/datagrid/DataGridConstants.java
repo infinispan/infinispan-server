452e25a52a77ca59c3c9a0652fac62b67a99f64f
Trustin Lee <trustin@gmail.com>
2011-05-31 06:09:47 +0000

    Updated copyright headers

--- integration/src/main/java/org/jboss/datagrid/DataGridConstants.java
+++ integration/src/main/java/org/jboss/datagrid/DataGridConstants.java
@@ -1,9 +1,5 @@
 /*
- * JBoss, Home of Professional Open Source
- *
- * Copyright 2011, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution
- * for a full listing of individual contributors.
+ * Copyright 2011 Red Hat, Inc. and/or its affiliates.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -12,13 +8,13 @@
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 package org.jboss.datagrid;
 
@@ -57,7 +53,7 @@ public class DataGridConstants {
     public static final String CF_CACHEMANAGER = "infinispan-configuration.xml";
     public static final String CF_HOTROD = "datagrid-endpoint-hotrod.properties";
     public static final String CF_MEMCACHED = "datagrid-endpoint-memcached.properties";
-    
+
     private DataGridConstants() {
         // Constant table
     }
--- integration/src/main/java/org/jboss/datagrid/SecurityActions.java
+++ integration/src/main/java/org/jboss/datagrid/SecurityActions.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2011 Red Hat, Inc. and/or its affiliates.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
 package org.jboss.datagrid;
 
 import java.security.AccessController;
@@ -6,8 +24,7 @@ import java.security.PrivilegedAction;
 /**
  * Package privileged actions
  *
- * @author Scott.Stark@jboss.org
- * @version $Id $
+ * @author <a href="mailto:Scott.Stark@jboss.org">Scott Stark</a>
  */
 public class SecurityActions {
     interface TCLAction {
@@ -98,8 +115,8 @@ public class SecurityActions {
             @Override
             public void setContextClassLoader(final Thread thread,
                     final ClassLoader cl) {
-                AccessController.doPrivileged(
-                        new PrivilegedAction<ClassLoader>() {
+                AccessController
+                        .doPrivileged(new PrivilegedAction<ClassLoader>() {
                             @Override
                             public ClassLoader run() {
                                 thread.setContextClassLoader(cl);
