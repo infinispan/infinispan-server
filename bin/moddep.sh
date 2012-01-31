fa894788919fe7797fec76bc48f41e987db0e7a2
Tristan Tarrant <ttarrant@redhat.com>
2012-01-31 10:56:31 +0100

    Reorganized build 	One module per edition (server,standalone,embedded) 	Assembling moved to parent module

--- bin/moddep.sh
+++ bin/moddep.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 DIRNAME=`dirname "$0"`
 BASEDIR=$DIRNAME/..
-MODULES=`find $BASEDIR -name 'module.xml'`
-$DIRNAME/moduleTools.py $@ $DIRNAME/../dist-dir/src/main/resources/standalone/configuration/standalone.xml $MODULES
+MODULES=`find $BASEDIR/dist-dir/target -name 'module.xml'`
+$DIRNAME/moduleTools.py $@ $BASEDIR/dist-dir/src/main/resources/standalone/configuration/standalone.xml $MODULES
 
-# moddep.sh | tr . / | sed 's/\(.*\)/modules\/\1\/main/' | xargs rm -rf
+# moddep.sh | tr . / |  | xargs rm -rf
 # find . -type d -empty -delete
