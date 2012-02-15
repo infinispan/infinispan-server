f8f0b918cc9404449c159e0864c49e1f1409f17a
Tristan Tarrant <ttarrant@redhat.com>
2012-02-15 16:16:34 +0100

    Update trim list

--- bin/moddep.sh
+++ bin/moddep.sh
@@ -4,5 +4,4 @@ BASEDIR=$DIRNAME/..
 MODULES=`find $BASEDIR/server/target -name 'module.xml'`
 $DIRNAME/moduleTools.py $@ $BASEDIR/server/src/main/resources/standalone/configuration/standalone.xml $MODULES
 
-# moddep.sh | tr . / |  | xargs rm -rf
-# find . -type d -empty -delete
+# moddep.sh | tr . / | sed 's/$/\/main\/\*\*/' > trim-modules.txt
