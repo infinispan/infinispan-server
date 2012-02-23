590753eed377e77d5c0cc37a2129a25c18d7098c
Tristan Tarrant <ttarrant@redhat.com>
2012-02-23 09:34:49 +0100

    Build against AS7.1.0.Final-prerelease2

--- bin/moddep.sh
+++ bin/moddep.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 DIRNAME=`dirname "$0"`
 BASEDIR=$DIRNAME/..
-MODULES=`find $BASEDIR/dist-dir/target -name 'module.xml'`
-$DIRNAME/moduleTools.py $@ $BASEDIR/dist-dir/src/main/resources/standalone/configuration/standalone.xml $MODULES
+MODULES=`find $BASEDIR/server/target -name 'module.xml'`
+$DIRNAME/moduleTools.py $@ $BASEDIR/server/src/main/resources/standalone/configuration/standalone.xml $MODULES
 
 # moddep.sh | tr . / |  | xargs rm -rf
 # find . -type d -empty -delete
