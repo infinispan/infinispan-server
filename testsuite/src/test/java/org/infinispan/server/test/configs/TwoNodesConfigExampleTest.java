f15ccd357f4aa144919631a5e8d0d4324ee545ed
Tomas Sykora <tsykora@redhat.com>
2013-09-20 13:03:06 +0200

    TwoNodesExampleTest fix, rm licences in others

--- testsuite/src/test/java/org/infinispan/server/test/configs/TwoNodesConfigExampleTest.java
+++ testsuite/src/test/java/org/infinispan/server/test/configs/TwoNodesConfigExampleTest.java
@@ -1,21 +1,3 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
- * as indicated by the @author tags. All rights reserved.
- * See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This copyrighted material is made available to anyone wishing to use,
- * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU Lesser General Public License, v. 2.1.
- * This program is distributed in the hope that it will be useful, but WITHOUT A
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
- * You should have received a copy of the GNU Lesser General Public License,
- * v.2.1 along with this distribution; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA  02110-1301, USA.
- */
 package org.infinispan.server.test.configs;
 
 import java.net.Inet6Address;
@@ -31,6 +13,7 @@ import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.server.test.client.rest.RESTHelper;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -108,6 +91,7 @@ public class TwoNodesConfigExampleTest {
     }
 
     @Test
+    @InSequence(1)
     public void testTwoNodesConfigExample() throws Exception {
         RemoteInfinispanCache ric = server1.getCacheManager(CACHE_MANAGER_NAME).getCache(DEFAULT_CACHE_NAME);
         RemoteInfinispanCache ric2 = server2.getCacheManager(CACHE_MANAGER_NAME).getCache(DEFAULT_CACHE_NAME);
