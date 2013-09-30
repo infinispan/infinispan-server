1efc826ab6c2435afd0b023eace288707c1d1676
Tristan Tarrant <ttarrant@redhat.com>
2013-09-30 10:58:41 +0200

    ISPN-3570 Make sure the awaitInitialStateTransfer property is actually applied

--- infinispan/src/main/java/org/jboss/as/clustering/infinispan/subsystem/SharedStateCacheAdd.java
+++ infinispan/src/main/java/org/jboss/as/clustering/infinispan/subsystem/SharedStateCacheAdd.java
@@ -51,10 +51,12 @@ public abstract class SharedStateCacheAdd extends ClusteredCacheAdd {
             ModelNode stateTransfer = cache.get(ModelKeys.STATE_TRANSFER, ModelKeys.STATE_TRANSFER_NAME);
 
             final boolean enabled = StateTransferResource.ENABLED.resolveModelAttribute(context, stateTransfer).asBoolean();
+            final boolean awaitInitialTransfer = StateTransferResource.AWAIT_INITIAL_TRANSFER.resolveModelAttribute(context,  stateTransfer).asBoolean();
             final long timeout = StateTransferResource.TIMEOUT.resolveModelAttribute(context, stateTransfer).asLong();
             final int chunkSize = StateTransferResource.CHUNK_SIZE.resolveModelAttribute(context, stateTransfer).asInt();
 
             builder.clustering().stateTransfer().fetchInMemoryState(enabled);
+            builder.clustering().stateTransfer().awaitInitialTransfer(awaitInitialTransfer);
             builder.clustering().stateTransfer().timeout(timeout);
             builder.clustering().stateTransfer().chunkSize(chunkSize);
         }
