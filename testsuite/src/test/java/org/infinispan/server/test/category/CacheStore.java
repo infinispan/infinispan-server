8c113d0bab649168f51aec665bcae9ae1b71499f
jmarkos <jmarkos@redhat.com>
2013-11-13 20:38:21 +0100

    Asymmetric cluster test

--- testsuite/src/test/java/org/infinispan/server/test/category/CacheStore.java
+++ testsuite/src/test/java/org/infinispan/server/test/category/CacheStore.java
@@ -3,7 +3,7 @@ package org.infinispan.server.test.category;
 import org.junit.experimental.categories.Category;
 
 /**
- * {@link Category} tag for clustered client tests.
+ * {@link Category} tag for cache store tests.
  * 
  * @author <a href="mailto:mlinhard@redhat.com">Michal Linhard</a>
  * 
